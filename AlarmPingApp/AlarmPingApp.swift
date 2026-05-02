d6b2a0775d46b6dfd737313d7e2a9d4e1061f048
happyboy457scratch-sudo <happyboy457scratch@gmail.com>
2026-05-02 13:43:49 -0500

    Initialize AlarmPingApp with SwiftUI structure

---
+++ AlarmPingApp/AlarmPingApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct AlarmPingApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
